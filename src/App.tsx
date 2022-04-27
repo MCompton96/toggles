3e7a27789cf84e4186ee2841957db6ade092bf43
MCompton96 <69015328+MCompton96@users.noreply.github.com>
2022-04-27 14:22:31 +0100

    Initial create react app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Toggles from './Components/Toggles/Toggles';
+
+function App() {
+  return (
+    <div className="App">
+      <Toggles />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Toggles/Toggles.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+const Toggles = () => {
+    return (
+        <div>Hello World</div>
+    )
+};
+
+export default Toggles;
